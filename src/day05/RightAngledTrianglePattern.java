c5ecc6999382fd8a3956acb6a674fc435446a946
rajendraprasad-anchala <rajendraprasad.anchala@gmail.com>
2022-06-19 16:02:57 +0800

    * Pattern Program Added

---
+++ src/day05/RightAngledTrianglePattern.java
@@ -0,0 +1,19 @@
+package day05;
+
+public class RightAngledTrianglePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i =10;
+		
+		for (int j=1;j<=i;j++) {
+			for (int k=1; k<=j;k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+				
+	}
+
+}
